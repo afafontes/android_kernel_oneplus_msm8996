989c410cb09763907dcb93b5662b7f99305998e2
André Fontes <andrefontes14@hotmail.com>
2017-09-30 17:22:25 +0100

    drivers: wakeup: disable timerfd, netlink and wlan wakelocks
    wlan_wow_wl, wlan and netmgr_wl wakelocks are disabled by default.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -19,6 +19,13 @@
 
 #include "power.h"
 
+static bool enable_wlan_ws = true;
+module_param(enable_wlan_ws, bool, 0644);
+static bool enable_timerfd_ws = true;
+module_param(enable_timerfd_ws, bool, 0644);
+static bool enable_netlink_ws = true;
+module_param(enable_netlink_ws, bool, 0644);
+
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
  * if wakeup events are registered during or immediately before the transition.
@@ -421,6 +428,23 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 {
 	unsigned int cec;
 
+	if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", 6)) ||
+ 		(!enable_wlan_extscan_wl_ws &&
+ 			!strncmp(ws->name, "wlan_extscan_wl", 15)) ||
+ 		(!enable_qcom_rx_wakelock_ws &&
+			!strncmp(ws->name, "qcom_rx_wakelock", 16)) ||
+		(!enable_wlan_ws &&
+                        !strncmp(ws->name, "wlan", 4)) ||
+		(!enable_timerfd_ws &&
+                        !strncmp(ws->name, "[timerfd]", 9)) ||
+		(!enable_netlink_ws &&
+                        !strncmp(ws->name, "NETLINK", 7))) {
+ 		if (ws->active)
+ 			wakeup_source_deactivate(ws);
+ 
+ 		return;
+	}
+	
 	/*
 	 * active wakeup source should bring the system
 	 * out of PM_SUSPEND_FREEZE state
